cd33a086ebee97cafb830d6f591c374662d6973d
Martin Marguerat <49449909+margueratm@users.noreply.github.com>
2021-03-23 13:51:28 +0100

    Improvements

--- MP_3-0-i/main.asm
+++ MP_3-0-i/main.asm
@@ -2,7 +2,7 @@
 ; * @file   MP_3-0-i/main.asm
 ; * @author	Martin MARGUERAT
 ; * @date	23-03-2021
-; * @brief  Calculate a division and a modulo value
+; * @brief  Calculate a quotient and a modulus value of 2 words
 ; ****************************************************************************/
 
         .cdecls C,LIST,"msp430.h"       ; Include device header file
@@ -15,7 +15,7 @@
 		.align 2						; specify a word alignement
 
 Quotient:	.space	2					; reserve 1 word for variable Quotient
-Reste:		.space	2					; reserve 1 word for variable Reste
+Modulus:	.space	2					; reserve 1 word for variable Reste
 
 ;-------------------------------------------------------------------------------
         .text                           ; Assemble into program memory
@@ -33,16 +33,16 @@ StopWDT: mov.w   #WDTPW|WDTHOLD,&WDTCTL ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 main:	nop
-            mov.w	#2, r6				; X value
-            mov.w	#11, r7				; Y value
-            mov.w	#0x0, r8                 ; index
+            mov.w	#0x15AF, r6				; X value
+            mov.w	#0x4FFF, r7				; Y value
+            mov.w	#0x0, r8                ; index
 divide:		cmp.w	r6, r7
 			jl		end						; jump to end if y < x
 			sub.w	r6, r7					; y = y-x
 			inc.w	r8						; i++
 			jmp		divide
 end:		mov.w	r8, Quotient			; i = quotient	Y/X
-			mov.w	r7, Reste				; y = reste		Y%X
+			mov.w	r7, Modulus				; y = modulus	Y%X
 loop:		jmp		loop					; garde-fou
 			nop
 
