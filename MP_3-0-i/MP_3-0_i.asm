227226b1fe7311bf2a1dd14f6a2cb96b6b94a91d
Martin Marguerat <49449909+margueratm@users.noreply.github.com>
2021-03-23 14:01:14 +0100

    Code improvements

--- MP_3-0-i/MP_3-0_i.asm
+++ MP_3-0-i/MP_3-0_i.asm
@@ -32,19 +32,19 @@ StopWDT: mov.w   #WDTPW|WDTHOLD,&WDTCTL ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-main:		nop
-            mov.w	#0x15AF, r6				; X value
-            mov.w	#0x4FFF, r7				; Y value
-            mov.w	#0x0, r8                ; index
-divide:		cmp.w	r6, r7
-			jl		end						; jump to end if y < x
-			sub.w	r6, r7					; y = y-x
-			inc.w	r8						; i++
-			jmp		divide
-end:		mov.w	r8, Quotient			; i = quotient	Y/X
-			mov.w	r7, Modulus				; y = modulus	Y%X
-loop:		jmp		loop					; garde-fou
-			nop
+main:	nop
+        mov.w	#0x15AF, r6				; X value
+        mov.w	#0x4FFF, r7				; Y value
+        mov.w	#0x0, r8                ; index
+divide:	cmp.w	r6, r7
+		jl		end						; jump to end if y < x
+		sub.w	r6, r7					; y = y-x
+		inc.w	r8						; i++
+		jmp		divide
+end:	mov.w	r8, Quotient			; i = quotient	Y/X
+		mov.w	r7, Modulus				; y = modulus	Y%X
+loop:	jmp		loop					; garde-fou
+		nop
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
