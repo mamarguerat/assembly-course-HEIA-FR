5139922940be3d54c6cf3beaf8abc1394e77aaf1
Martin Marguerat <49449909+margueratm@users.noreply.github.com>
2021-03-23 13:07:40 +0100

    Add exercice 4.7 + beginning of 4.8

--- TP2_Martin_Marguerat_Vincent_Schupbach/src/tp2_martin_marguerat_vincent_schupbach.asm
+++ TP2_Martin_Marguerat_Vincent_Schupbach/src/tp2_martin_marguerat_vincent_schupbach.asm
@@ -83,18 +83,53 @@ loop:
         nop                             ; to show result before next instr.
 
 ; - TP2: 4.7 Instr. Logiques
+		mov.b	#0x42, R6				; positive byte
+		and.b	#0xF0, R6				; bit3..0 -> 0 => R6=0x40
+		mov.b	#0x42, R6				; positive byte
+		bic.b	#0x0F, R6				; bit3..0 clear => R6=0x40
+		mov.w	#0x5555, R6				; positive word
+		xor.w	#0xFFFF, R6				; bit15..0 invert => R6=0xAAAA
+		bis.w	#0xF000, R6				; bit15..12 set => R6=0xFAAA
+		mov.w	#0xFFEE, R6				; negative word
+		bit.w	#0x8000, R6				; bit15 test
+        bic.w	#0xFFFF, SR				; Clear all SR bits
+        nop                             ; to show result before next instr.
+
 
 ; - TP2: 4.8 Additions/Soustractions signé 16bits
         ; a) Addition
+		mov.w	#0x7FFF, R6				; positive word
+		add.w	#0x0001, R6				; Addition avec dépassement sans limitation: R6=0x8000
+		; V=1, N=1
+        bic.w	#0xFFFF, SR				; Clear all SR bits
+        nop                             ; to show result before next instr.
 
         ; b) Soustraction
+		mov.w	#0x8000, R6				; negative word
+		sub.w	#0x0001, R6				; Soustration avec dépassement sans limitation: R6=0x7FFF
+		; V=1, N=0
+        bic.w	#0xFFFF, SR				; Clear all SR bits
+        nop                             ; to show result before next instr.
 
         ; c) Limitation de l'addition au valeur max
+		mov.w	#0x7FFF, R6				; positive word
+		add.w	#0x0001, R6				; Addition avec dépassement sans limitation
+		jl		limitAdd				; S'il y a dépassement, limiter l'addition
+		jn		limitAdd				; Si le nombre est négatif, limiter l'addition
+finishAdd:
+        bic.w	#0xFFFF, SR				; Clear all SR bits
+        nop                             ; to show result before next instr.
 
         ; d) Limitation la soustraction au min value
-
+		mov.w	#0x8000, R6				; negative word
+		sub.w	#0x0001, R6				; Soustration avec dépassement sans limitation
+		jge		finishSub				; S'il y a pas de dépassement, sauter à la fin de la soustraction
+		jn		finishSub				; Si le nombre est négatif, sauter à la fin de la soustraction
+		mov.w	#0x8000, R6				; Limitation de la soustraction
+finishSub:
+        bic.w	#0xFFFF, SR				; Clear all SR bits
+        nop                             ; to show result before next instr.
         ; e) Opérations sur 32bits
-
 
 
 
@@ -102,7 +137,13 @@ loop:
 
         jmp loop                        ; jump to current location '$'
                                         ; (endless loop)
+limitAdd:
+		mov.w	#0x7FFF, R6				; Limitation de l'addition
+		jmp		finishAdd				; sauter à la fin de l'addition
         nop                             ; necessary for MSP430FR5969
+
+
+
 
 
 ;-------------------------------------------------------------------------------
