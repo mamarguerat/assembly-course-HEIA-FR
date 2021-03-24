6373a7fc312a5c53b3d0e960699213c21aec36ee
Martin Marguerat <martin@marguerat.ch>
2021-03-24 16:18:06 +0100

    Add exercise 3.0 f)

---
+++ MP_3-0-ASM-f/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+ASM_SRCS += \
+../MP_3-0_f.asm 
+
+OBJS += \
+./MP_3-0_f.obj 
+
+ASM_DEPS += \
+./MP_3-0_f.d 
+
+OBJS__QUOTED += \
+"MP_3-0_f.obj" 
+
+ASM_DEPS__QUOTED += \
+"MP_3-0_f.d" 
+
+ASM_SRCS__QUOTED += \
+"../MP_3-0_f.asm" 
+
+
--- MP_3-0-ASM-f/MP_3-0_f.asm
+++ MP_3-0-ASM-f/MP_3-0_f.asm
@@ -16,7 +16,7 @@
                                             ; and retain current section.
             .retainrefs                     ; And retain any sections that have
                                             ; references to current section.
-message:    .cstring "~Bienvenue!"
+message:    .cstring "Coucou"
 
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
@@ -27,22 +27,23 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
-			mov.w #message, r7				; adresse 1ere val.
-
-bcl:
-			mov.b @r7+,r5					; déplacer la lettre à l'adresse r7, puis r7++
-			cmp.b #0,r5						; Comparer à la valeur 0 (caractère '\0' => fin de string)
-        	jnz p1							; 	si r5!=0 jump p1
-        	jmp fin							; 	sinon (r5=0) jump fin
-p1:         cmp.b #'a',r5					; Comparer à la valeur 0x61 (caractère 'a')
-            jhs p2							; 	si r5>=0x61 jump p2
-            jmp bcl							; 	sinon (r5<0x61) jump fin
-p2:      	cmp.b #'z'+1,r5					; Comparer à la valeur 0x7B+1 (caractère 'z'+1)
-            jlo p3							; 	si r5<0x7E jump p3
-            jmp bcl							; 	sinon (r5>=0x7E) jump fin
-p3:			sub.b #('a'-'A'),r5				; Soustraire la valeur 0x20 ('a'-'A' => 0x61-0x41 = 0x20)
-			mov.b r5,-1(r7)					; Déplacer le nouveau caractère à l'adresse r7-1
-			jmp bcl							; Recommencer la boucle
+			mov.w	#message, r7			; adresse 1ere val.
+			jmp		bcl1					; choix de l'algorithme
+bcl1:
+			mov.b	@r7+,r5					; déplacer la lettre à l'adresse r7, puis r7++
+			cmp.b	#0,r5					; Comparer à la valeur 0 (caractère '\0' => fin de string)
+        	jnz		p1						; 	si r5!=0 jump p1
+        	jmp		fin						; 	sinon (r5=0) jump fin
+p1:         mov.b	@r7, -1(r7)				; Copier la valeur i+1 dans i
+			jmp 	bcl1					; Recommencer la boucle
+bcl2:
+			mov.b	@r7+,r5					; déplacer la lettre à l'adresse r7, puis r7++
+			cmp.b	#0,r5					; Comparer à la valeur 0 (caractère '\0' => fin de string)
+        	jnz		p2						; 	si r5!=0 jump p1
+        	jmp		fin						; 	sinon (r5=0) jump fin
+p2:         inc.b	r5						; Ajouter 1 à la valeur de r5
+            mov.b	r5,-1(r7)				; Déplacer le nouveau caractère à l'adresse r7-1
+			jmp 	bcl2					; Recommencer la boucle
 
 
 fin:		jmp		fin
