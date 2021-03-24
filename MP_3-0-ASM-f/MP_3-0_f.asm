9c7380cbaba00ee2d96fdfa37d1e21b4d45cff84
Martin Marguerat <martin@marguerat.ch>
2021-03-24 16:22:58 +0100

    Comment improvements

--- MP_3-0-ASM-f/MP_3-0_f.asm
+++ MP_3-0-ASM-f/MP_3-0_f.asm
@@ -34,14 +34,14 @@ bcl1:
 			cmp.b	#0,r5					; Comparer à la valeur 0 (caractère '\0' => fin de string)
         	jnz		p1						; 	si r5!=0 jump p1
         	jmp		fin						; 	sinon (r5=0) jump fin
-p1:         mov.b	@r7, -1(r7)				; Copier la valeur i+1 dans i
+p1:         mov.b	@r7, -1(r7)				; Copier la valeur à l'index i+1 dans l'index i (=> x[i] = x[i+1])
 			jmp 	bcl1					; Recommencer la boucle
 bcl2:
 			mov.b	@r7+,r5					; déplacer la lettre à l'adresse r7, puis r7++
 			cmp.b	#0,r5					; Comparer à la valeur 0 (caractère '\0' => fin de string)
-        	jnz		p2						; 	si r5!=0 jump p1
+        	jnz		p2						; 	si r5!=0 jump p2
         	jmp		fin						; 	sinon (r5=0) jump fin
-p2:         inc.b	r5						; Ajouter 1 à la valeur de r5
+p2:         inc.b	r5						; Ajouter 1 à la valeur de r5 (=> x[i] = x[i] + 1)
             mov.b	r5,-1(r7)				; Déplacer le nouveau caractère à l'adresse r7-1
 			jmp 	bcl2					; Recommencer la boucle
 
