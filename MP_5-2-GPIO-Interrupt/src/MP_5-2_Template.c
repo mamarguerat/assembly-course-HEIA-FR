4020e1eee7da4f972cb5cf039522923f79745fe8
Martin Marguerat <martin@marguerat.ch>
2021-04-20 13:11:40 +0200

    Add chapter 5

---
+++ MP_5-2-GPIO-Interrupt/src/MP_5-2_Template.c
@@ -0,0 +1,54 @@
+/**
+  ****************************************************************************
+  * @file    MP_5-2.c
+  * @author  Scherwey Roland
+  * @date    17.04.2015
+  * @brief   GPIO Interrupt handling
+  *
+  *
+  * @note    Following ULP's deactivated: 1.1, 4.1, 5.1, 5.2, 5.3, 12.2, 13.1
+  *          Optimization level: Off
+  *
+  ****************************************************************************/
+
+#include <msp430.h>
+
+//----------------------------------------------------------------------------
+// local definitions
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// local functions
+//----------------------------------------------------------------------------
+
+int main( void )
+{
+  WDTCTL = WDTPW + WDTHOLD;// Stop watchdog timer to prevent time out reset
+  PM5CTL0 &= ~LOCKLPM5;    // Disable the GPIO power-on default high-impedance mode
+                           // to activate previously configured port settings
+
+  // Configure GPIO to avoid warnings
+  PAOUT = PBOUT = PJOUT = 0;
+  PADIR = PBDIR = 0;
+
+
+
+  // a) Define P1.0 as output
+
+  // b) Define P1.1 (S2) as input
+
+  // c) Configure P1.1 to generate interrupt on falling edge
+  
+  __bis_SR_register(GIE);           // enable interrupts
+
+
+  for (;;);
+}
+
+
+// Port 1 interrupt service routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1_isr(void)
+{
+  // d) toggle LED2
+}
---
+++ MP_5-20-Timer-initialization/src/HAL/HAL_MSP430.c
@@ -0,0 +1,95 @@
+/**
+  ****************************************************************************
+  * @file    HAL_MSP430.c
+  * @author  Scherwey Roland
+  * @date    25.04.2015
+  * @brief   Implementation of MSP430 HAL (Hardware Abstraction Layer)
+  *
+  ****************************************************************************/
+
+//----------------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------------
+#include "HAL_MSP430.h"
+
+
+#include <msp430.h>
+
+
+//----------------------------------------------------------------------------
+// local definitions
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// local functions
+//----------------------------------------------------------------------------
+
+/// @brief  This method initializes the Clock System
+/// @param  -
+void initCS(void)
+{
+  // Configure one FRAM waitstate as required by the device datasheet for MCLK
+  // operation beyond 8MHz _before_ configuring the clock system.
+  FRCTL0 = FRCTLPW | NWAITS_0;
+
+  CSCTL0_H = CSKEY_H;                       // Unlock CS registers
+
+  CSCTL1   = DCOFSEL_0;                     // Set DCO to 1MHz
+  CSCTL2   = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;  // Set ACLK=VLOCLK and SMCLK = MCLK = DCO
+  CSCTL3   = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 (register cleared to 0...)
+
+  CSCTL0_H = 0;                             // Lock CS registers
+
+}
+
+/// @brief  This method initializes the GPIO's - also check Timer usage
+/// @param  -
+void initGPIO(void)
+{
+  // Disable the GPIO power-on default high-impedance mode to activate
+  // previously configured port settings
+  PM5CTL0 &= ~LOCKLPM5;
+
+  // Configure GPIO
+  PAOUT = PBOUT = PJOUT = 0;
+  PADIR = PBDIR = 0;
+
+  P4DIR &= ~BIT5; // P4.5 (S1) input (is already configured as input after reset...)
+  P4REN |=  BIT5; // Enable pull-up/pull-down on P4.5 (S1)
+  P4OUT |=  BIT5; // Configure pull-up on P4.5 (S1) (see MSP430FR5969 schematics)
+
+  // LED handling
+  P1DIR |= BIT0;        // P1.0 output
+  P4DIR |= BIT6;        // P4.6 output
+}
+
+void initTimer(void)
+{
+  TA0CCTL0  &= ~CCIE;             // TA0CCR0 interrupt disabled
+
+  TA0CTL   =  MC__STOP            // Stop timer first...
+           |  TACLR;              // ... and clear it
+
+#warn TA0CTL to be configured according exercice
+
+  TA0CCTL0 |= CCIE;               // TA0CCR0 interrupt enabled
+}
+
+
+//----------------------------------------------------------------------------
+// Implementations
+//----------------------------------------------------------------------------
+
+void initChip(void)
+{
+  WDTCTL = WDTPW | WDTHOLD; // Stop WDT
+
+  initGPIO(); // Configure GPIO
+
+  initCS();   // Startup clock system with max DCO setting ~8MHz
+
+  initTimer(); // used for periodically wake-up from Low Power
+
+}
+
+
